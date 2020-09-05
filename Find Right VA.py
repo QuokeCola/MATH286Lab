3ff60e2ac07226c8ab093b2fc114040952c42010
Qian Chen <QuokeCola@iCloud.com>
2020-09-05 14:48:59 +0800

    A lot of functions

--- Find Right VA.py
+++ Find Right VA.py
@@ -1,4 +1,6 @@
 from __future__ import division
+import multiprocessing
+import sys
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -9,16 +11,17 @@ from RungKutta_Method import *
 from analytical import *
 from Function_DF import *
 
-import multiprocessing
-
-import sys
-
 import matplotlib.pyplot as plt
 import numpy as np
+sys.setrecursionlimit(100000)
 
 start_Point = (0, 1)
-Default_Step = 0.01
-sys.setrecursionlimit(100000)
+Step = 0.01
+EndScale = 0.85
+
+Analytical_Subjects = 1000
+Analytical_Points = 100
+
 
 def diffF(x,y):
     return y*y+x*y+x*x
@@ -34,19 +37,28 @@ def Single_Calculation(Step, end_Scale):
 
 def showRes(ResTup):
     print('-----------------Trial '+str(ResTup[3]) +'-----------------')
-    if (str(ResTup[0][0][1][-1]) == 'inf'):
+    if not (str(ResTup[0][0][1][-1]).find('inf') == -1):
         print('[\033[1;31mWarning\033[0m] Euler Overflow')
     else:
         print('Euler: ' + str(ResTup[0][0][1][-1]))
 
-    if (str(ResTup[1][0][1][-1]) == 'inf'):
+    if not (str(ResTup[1][0][1][-1]).find('inf') == -1):
         print('[\033[1;31mWarning\033[0m] Advanced Euler Overflow')
     else:
         print('Opt Euler: ' + str(ResTup[1][0][1][-1]))
-    if (str(ResTup[2][0][1][-1]) == 'inf'):
+    if not (str(ResTup[2][0][1][-1]).find('inf') == -1):
         print('[\033[1;31mWarning\033[0m] Rung Kutta Overflow')
     else:
         print('Rung Kutta: ' + str(ResTup[2][0][1][-1]))
+
+def log_Res(Res):
+    file = open('result.csv','w')
+    header = 'x,Euler, Advanced Euler, Rung Kutta\n'
+    lines = [header]
+    for i in range(len(Res[0][0][0])):
+        lines.append(str(Res[2][0][0][i])+','+ str(Res[0][0][1][i])+ ',' + str(Res[1][0][1][i])+ ','+str(Res[2][0][1][i])+ '\n')
+    file.writelines(lines)
+    file.close()
 
 if __name__ == '__main__':
     # Initialize
@@ -56,49 +68,46 @@ if __name__ == '__main__':
 
     # Run Calculation
     print("[\033[1;33mInfo\033[0m] Start Computing Numerical Results")
-    y = start_Point[1]
-    x = start_Point[0]+Default_Step * 10
-    Step = Default_Step
-    trial = 0
-    # while Step > 0.001:
-    #     print('[\033[1;32mInfo\033[0m]Running Trial' + str(trial))
-    #     y = 0
-    #     while not (str(y) == 'inf' or str(y) == '-inf'):
-    #         x += Step
-    #         Res = Single_Calculation(0.001, x)
-    #         y = Res[2][0][1][-1]
-    #         print('[\033[1;32mStatus\033[0m]On range ' + str(x))
-    #     x = x - Step
-    #     Step = Step / 2
-    #     trial += 1
-    # x -= (Step*2)
-    # Res = Single_Calculation(Step, x)
-    Res = Single_Calculation(0.000001, 0.858876)
-    showRes(Res)
-    # for i in np.linspace(0.8588867,0.828800,68):
-    # for res in FinalRes:
-    #     showRes(res)
+    Res=Single_Calculation(Step,EndScale)
     print("[\033[1;32mInfo\033[0m] Numerical Compute Finished")
+    print('[\033[1;33mInfo\033[0m] Start log Results')
+    log_Res(Res)
+    print('[\033[1;32mInfo\033[0m] Log Results Finished')
 
     # Run Analytical Results
+    analy = analytical()
+    analy.generate_subs(Analytical_Subjects)
+    anares = analy.compute(EndScale, Analytical_Points)
 
-    # analy = analytical()
-    # analy.generate_subs(2000)
-    # AnaRes = analy.compute(0.88, 1000)
-    # # for i in range(len(AnaRes[0])):
-    # #     print(AnaRes[1][i])
-    # #     if str(AnaRes[1][i]) == 'inf':
-    # #         print(AnaRes[0][i-1])
-    # #         break
-    # # Plot Direction Field
-    # funcDF = FunDF(diffF,-5,5,-5,10000,100,100)
-    # funcDF.generate()
-    #
-    # # Plot Points
-    # plt.scatter(Res[0][0][0],Res[0][0][1],color = 'blue', label = 'Euler')
-    # plt.scatter(Res[1][0][0],Res[1][0][1],color = 'red', label = 'OptEuler')
-    # plt.scatter(Res[2][0][0],Res[2][0][1],color = 'green', label = 'Rung Kutta')
-    # plt.scatter(AnaRes[0], AnaRes[1], color ='purple', label ='Analysis')
-    # plt.legend(loc = 'best')
-    # plt.show()
-    # print("[\033[1;32mInfo\033[0m] Plot Generated")
+    # Plot Direction Field
+    LowX = Res[0][0][0][0]
+    HighX = Res[0][0][0][-1]
+    LowY = Res[2][0][1][0]
+    HighY = start_Point[1]
+    for i in range(len(Res[2][0][1])):
+
+        if not str(Res[2][0][1][i]).find('inf') == -1:
+            HighY = Res[2][0][1][i-1]
+            print(HighY)
+            break
+    for i in range(len(anares[1])):
+        if not str(anares[1][i]).find('inf') == -1:
+            HighY = anares[1][i-1]
+            print(HighY)
+            break
+    if HighY == start_Point[1]:
+        HighY = anares[1][-1]
+    funcDF = FunDF(diffF,LowX,HighX,LowY,HighY,20,20)
+    funcDF.generate()
+
+    # Plot Points
+    print("[\033[1;33mInfo\033[0m] Add points to plot")
+    plt.scatter(Res[0][0][0],Res[0][0][1],color = 'blue', label = 'Euler')
+    plt.scatter(Res[1][0][0],Res[1][0][1],color = 'red', label = 'OptEuler')
+    plt.scatter(Res[2][0][0],Res[2][0][1],color = 'green', label = 'Rung Kutta')
+    plt.scatter(anares[0], anares[1], color ='purple', label ='Analysis')
+    plt.legend(loc = 'best')
+    print("[\033[1;32mInfo\033[0m] Points added to plot")
+    print("[\033[1;33mInfo\033[0m] Generating plot")
+    plt.show()
+    print("[\033[1;32mInfo\033[0m] Plot Generated")
--- Function_DF.py
+++ Function_DF.py
@@ -13,16 +13,17 @@ class FunDF:
         
     def generate(self):
         print("[\033[1;33mInfo\033[0m] Initializing Meshes")
-        x, y = np.meshgrid(np.linspace(-5, 5, 20), np.linspace(-5, 5, 20))
-        u = np.linspace(1, 1, 400)
-        u = u.reshape(20, 20)
-        v = np.linspace(0, 0, 400)
-        v = v.reshape(20, 20)
+        x, y = np.meshgrid(np.linspace(self.low_x, self.high_x, self.x_res), np.linspace(self.low_y, self.high_y, self.y_res))
+        u = np.linspace(1, 1, self.x_res*self.y_res)
+        u = u.reshape(self.x_res, self.y_res)
+        v = np.linspace(0, 0, self.x_res*self.y_res)
+        v = v.reshape(self.x_res, self.y_res)
         print("[\033[1;32mInfo\033[0m] Successfully initialize Meshes")
         print("[\033[1;33mInfo\033[0m] Initializing Direction Field")
-        for i in range(0, 20):
-            for j in range(0, 20):
-                v[i][j] = (self.func(x[i][j], y[i][j])) / math.sqrt(1 + self.func(x[i][j], y[i][j]) * self.func(x[i][j], y[i][j]))
+        for i in range(0, self.x_res):
+            for j in range(0, self.y_res):
+                v[i][j] = (self.func(x[i][j], y[i][j])) / math.sqrt(1 + self.func(x[i][j], y[i][j]) * self.func(x[i][j], y[i][j])) * (self.high_x - self.low_x)/(self.high_y - self.low_y)
                 u[i][j] = 1 / math.sqrt(1 + self.func(x[i][j], y[i][j]) * self.func(x[i][j], y[i][j]))
+        #plt.figure(figsize=(108, 81), dpi=100.0)
         plt.quiver(x, y, u, v)
         print("[\033[1;32mInfo\033[0m] Successfully initialize Direction Field")
